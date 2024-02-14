5738d3e253944585bdb906a97132ced1cd36d18b
HenriikRC <112691706+HenriikRC@users.noreply.github.com>
2024-02-14 15:38:37 +0100

    Added module Asteroid and Enemy and modified Player
    Asteroid Module skeleton added
    
    Enemy Module added and implemented:
         - Enemy.java: Enemy Entity
         - EnemyControlSystem.java: Controlling movement and shooting
         - EnemyPlugin.java: Construct and deconstruct of Enemy
    
    Player module modified:
          - PlayerPlugin.java: Modified the polygon of the Player to be akin to a spaceship.

---
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
@@ -0,0 +1,35 @@
+package dk.sdu.mmmi.cbse.enemysystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+import java.util.Random;
+
+public class EnemyPlugin implements IGamePluginService {
+
+    private Entity enemy;
+
+    @Override
+    public void start(GameData gameData, World world) {
+        enemy = createEnemyShip(gameData);
+        world.addEntity(enemy);
+    }
+
+    private Entity createEnemyShip(GameData gameData) {
+        Entity enemyShip = new Enemy();
+        enemyShip.setPolygonCoordinates(-5,-5,10,0,-5,5);
+        Random randomGenerator = new Random();
+        double randomX = randomGenerator.nextDouble(0, gameData.getDisplayHeight());
+        double randomY = randomGenerator.nextDouble(0, gameData.getDisplayWidth());
+        enemyShip.setX(randomX);
+        enemyShip.setY(randomY);
+        return enemyShip;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        world.removeEntity(enemy);
+    }
+}
